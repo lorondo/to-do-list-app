42b436cfc14953c43a80b03be11d8421760c931a
lorondo <mralexanderlindsay@gmail.com>
2024-09-05 13:37:40 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function newItem(){
+
+//javascript
+//1. Adding a new item to the list of items: 
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!")
+    } else {
+        $('#list').append(li);
+    }
+
+//2. Crossing out an item from the list of items:
+   function crossOut() {
+    li.classList.toggle("strike");
+    }
+
+    li.addEventListener("dblclick",crossOut);
+
+    jbfksdkjsb
